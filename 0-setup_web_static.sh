27011a622fb1a6293da3942d7d829703323f1b7a
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-09-30 01:36:23 +0100

    task1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,41 +1,30 @@
 #!/usr/bin/env bash
-# Configures my nginx server from scartch
-
+# Install Nginx if it not already installed
 if ! command -v nginx &> /dev/null;
 then
     sudo apt-get -y update
     sudo apt-get install -y nginx
     sudo service nginx start
 fi
-
-mkdir -p /data/
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
-
+# Create those folders if doesnt alreeady exist
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+#Create a fake HTML with simple content to test your Ngninx configuration
 echo "
-    <!DOCTYPE html>
-    <html lang='en'>
-        <head>
-            <title>Just a configuration script</title>
-            <meta charset="UTF-8"/>
-        </head>
-        <body>
-        <h1 style=color:'blue'>THIS IS MY HEADING</h1>
-            <p>Incoming devops, so get ready world!</P>
-        </body>
-    </html>
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
 " > /data/web_static/releases/test/index.html
-
+# Create a symbolik link that delete and recreated every time is ran
 source_file="/data/web_static/current"
 destiny_file="/data/web_static/releases/test/"
-
-ln -sf /data/web_static/releases/test/index.html /data/web_static/current
-
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
-
+sudo ln -sf "$destiny_file" "$source_file"
+# Give ownership of the /data/ to the ubuntu user and group recursive
+sudo chown -R ubuntu:ubuntu /data/
+# Update the Ninx configuration to work https://mydomainame.tech/hbnb_static
 sudo sed -i "38i \ \tlocation /hbnb_static/ {\n\t\talias $source_file/;\n\t\tautoindex off;\n\t}\n" /etc/nginx/sites-enabled/default
-
-service nginx restart
+sudo service nginx restart
