b2365586103e233756d57ad460545bda7b13635f
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-09-30 01:15:54 +0100

    task1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -28,29 +28,14 @@ echo "
     </html>
 " > /data/web_static/releases/test/index.html
 
+source_file="/data/web_static/current"
+destiny_file="/data/web_static/releases/test/"
+
 ln -sf /data/web_static/releases/test/index.html /data/web_static/current
 
 chown -R ubuntu /data/
 chgrp -R ubuntu /data/
 
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
+sudo sed -i "38i \ \tlocation /hbnb_static/ {\n\t\talias $source_file/;\n\t\tautoindex off;\n\t}\n" /etc/nginx/sites-enabled/default
 
 service nginx restart
